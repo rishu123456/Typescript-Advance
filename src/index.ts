071b16a6d7c04941347d866e566f933c73d89ed8
rishu123456 <rishu25112001@gamil.com>
2025-03-25 16:09:55 +0530

    Readonly property

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-interface user {
+interface User {
     name : string
     age : number
     email:string
-    address: string
-    id: number
+   
 }
 
-type user1 = Pick<user, 'name' | "age" | "email">
-
-type user2 =Partial<user1>
-
-function updateuser(updateprops:user2){
-    //hit the database to update only restricted parts
+const user :Readonly<User> = {
+    name:"rih",
+    age:12,
+    email:"rohsuup["
 }
+
+//user.name="rishu"
+//gives erroe we can not change even the internal value now earlier we were able to change like line no.-14 n
+// but with the help of Readonly api we can prevent this property which is usefull in many cases that can be found on the internet.
+
+
+
+// type user1 = Pick<user, 'name' | "age" | "email">
+
+// type user2 =Partial<user1>
+
+// function updateuser(updateprops:user2){
+//     //hit the database to update only restricted parts
+// }
 
 //defination- pick let us pick specific value from types and interface as well as above example
