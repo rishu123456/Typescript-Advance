3ef970c45c753c899494ec3522be24cd6b2f5315
rishu123456 <rishu25112001@gamil.com>
2025-03-25 15:43:15 +0530

    pick basics defination

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,5 @@ type user1 = Pick<user, 'name' | "age" | "email">
 function updateuser(updateprops:user1){
     //hit the database to update only restricted parts
 }
+
+//defination- pick let us pick specific value from types and interface as well as above example
